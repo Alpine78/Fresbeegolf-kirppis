31e95849070bb0af68ce627bdca1248788ec579d
Ilkka Rytkönen <ilkka@ilkansivu.net>
2019-04-12 18:28:43 +0300

    Ilmoituksen kuvat merge

--- resources/js/components/FrontPage.vue
+++ resources/js/components/FrontPage.vue
@@ -1,7 +1,11 @@
 <template>
     <b-container>
         <p>TERVE</p>
-
+        <!-- <b-link :to="{ name: 'demoadvert'}">
+        <search-result-item></search-result-item>
+        </b-link> -->
+        <h3>Tässäpä uusimmat ilmoitukset</h3>
+        <search-results></search-results>
     </b-container>
 </template>
 
